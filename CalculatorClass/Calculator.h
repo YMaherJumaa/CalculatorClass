4092b3c7a4e681c41723e4486dfa896e33d12329
YMaherJumaa <yara.maher@cis.asu.edu.eg>
2020-12-16 19:54:36 +0200

    addition and set for 2 numbers

---
+++ CalculatorClass/Calculator.h
@@ -0,0 +1,22 @@
+#pragma once
+class Calculator
+{
+private:
+	int number1, number2;
+public:
+	Calculator();
+	void setNumber1(int x)
+	{
+		number1 = x;
+	}
+	void setNumber2(int y)
+	{
+		number2 = y;
+	}
+	int addition()
+	{
+		return (number1 + number2);
+	}
+	~Calculator();
+};
+
