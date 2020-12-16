3bcef8e599d7776d29730c013bdecf143a800f2f
YMaherJumaa <yara.maher@cis.asu.edu.eg>
2020-12-16 20:16:42 +0200

    sub

--- CalculatorClass/Calculator.h
+++ CalculatorClass/Calculator.h
@@ -6,18 +6,9 @@ private:
 public:
 	Calculator();
 	//master branch
-	void setNumber1(int x)
-	{
-		number1 = x;
-	}
-	void setNumber2(int y)
-	{
-		number2 = y;
-	}
-	int addition()
-	{
-		return (number1 + number2);
-	}
+	void setNumber1(int x){number1 = x;}
+	void setNumber2(int y){number2 = y;}
+	int addition(){return (number1 + number2);}
 	//subtraction branch
 	int subtraction()
 	{
